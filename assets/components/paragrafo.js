3f215d86c328c63bd7cdd28e3138db383b1ec5a4
Ester Cubillos Matos <espamalu04@gmail.com>
2023-07-20 20:31:24 +0000

    add tudo

---
+++ assets/components/paragrafo.js
@@ -0,0 +1,26 @@
+import { LitElement, html, css } from "lit";
+
+export class Paragrafo extends LitElement {
+  static styles = [
+    css`
+      :host {
+        display: block;
+        color: var(--cor-secundria, #665e57);
+        font-family: var(--fonte-corpo);
+        font-size: 1rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 100%;
+      }
+
+      p {
+        margin: 0;
+      }
+    `,
+  ];
+
+  render() {
+    return html` <p><slot>Parágrafo</slot></p> `;
+  }
+}
+customElements.define("app-paragrafo", Paragrafo);
